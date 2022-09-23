3af02d3467da27895ee3e75fd678abd0de24b298
arnold-O <arnoldoghiator@gmail.com>
2022-09-24 00:39:09 +0100

    raw queries

--- Sequelize-101/route.js
+++ Sequelize-101/route.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const Sequelize = require("sequelize");
 
-
 const router = express.Router();
-
-
 
 const sequelize = new Sequelize("sql_class", "root", "", {
   host: "localhost",
@@ -54,66 +51,98 @@ const User = sequelize.define(
 
 // sync Model
 
-sequelize.sync();
-
-
+// sequelize.sync();
 
 // CREATE DATA TO TABLE
 router.post("/user", function (req, res) {
-    const { name, email, rollNo, status } = req.body;
+  const { name, email, rollNo, status } = req.body;
+
+  User.create({
+    name,
+    email,
+    rollNo,
+    status,
+  })
+    .then(function (data) {
+      res.status(200).json({
+        data,
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+router.get("/all-users", async (req, res) => {
+ try {
+  const getAllUsers = await User.findAll({
+    where: {
+      
+    },
+    
+  });
+  const UserInfo = getAllUsers.map((data) => {
+      return { name: data.name, email: data.email };
+    });
   
-    User.create({
+    res.status(200).json({
+      message: "success",
+      UserInfo,
+    });
+  
+ } catch (error) {
+  
+  res.status(500).json({
+    data:error
+  })
+  
+ }
+
+  
+});
+
+router.put("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, email, rollNo, status } = req.body;
+
+  await User.update(
+    {
       name,
       email,
       rollNo,
       status,
-    })
-      .then(function (data) {
-        res.status(200).json({
-          data,
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    },
+    {
+      where: {
+        id,
+      },
+    }
+  );
+  res.status(200).json({
+    message: "data successfuly updated",
   });
-  
-  router.get("/all-users", async(req, res) => {
-  
-      const getAllUsers = await User.findAll({
-          where:{
-              status:"0"
-          }
-      })
-  
-      const UserInfo = getAllUsers.map((data)=>{
-         
-          return {name:data.name, email:data.email}
-      })
-      
-    res.status(200).json({
-      message: "success",
-     UserInfo
-    });
+});
+router.delete("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  await User.destroy({
+    where: {
+      id,
+    },
   });
+  res.status(200).json({
+    message: "data successfuly deleted",
+  });
+});
 
-  router.put('/user/:id', async(req, res)=>{
-    const {id} = req.params
-    const { name, email, rollNo, status } = req.body;
 
-    const updateValue = await User.update({
-      name, email, rollNo, status
-     
-
-    },{
-      where:{
-        id
-      }
-    })
-      res.status(200).json({
-       message:"data successfuly updated"
-      })
-
+router.get('/raw-query', async(req, res)=>{
+  const rawQuery = await sequelize.query('select * from users',{
+    type:sequelize.QueryTypes.SELECT
   })
 
-  module.exports = router
+  res.status(200).json({
+    rawQuery
+  })
+})
+
+module.exports = router;
