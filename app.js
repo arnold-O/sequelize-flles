d1a9aa95e3db8b7c3a3f3b65079f14276b2e86e0
arnold-O <arnoldoghiator@gmail.com>
2022-09-27 23:19:35 +0100

    code refactor

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+
+
+
+const PORT = 4600
+
+
+app.get('/', (req, res)=>{
+    
+
+
+
+    res.send('we here')
+})
+
+app.listen(PORT, ()=>{
+    console.log(first)
+})
