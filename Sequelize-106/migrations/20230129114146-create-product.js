4bdff076725b5d3816381de3dd39677cc1c1e055
arnold-O <arnoldoghiator@gmail.com>
2023-01-30 10:16:53 +0100

    migrations stup

---
+++ Sequelize-106/migrations/20230129114146-create-product.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      status:{
+        type:Sequelize.ENUM('active', 'inactive'),
+        defaultValue:'active'
+      },
+      amount: {
+        type: Sequelize.INTEGER
+      },
+      // createdAt: {
+      //   allowNull: false,
+      //   type: Sequelize.DATE,
+      //   defaultValue:Sequelize.literal('CURRENT_TIMESTAMP')
+      // },
+      // updatedAt: {
+      //   allowNull: false,
+      //   type: Sequelize.DATE,
+      //   defaultValue:Sequelize.literal('CURRENT_TIMESTAMP')
+      // }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ Sequelize-106/migrations/20230129132520-create-category.js
@@ -0,0 +1,30 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      categoryImage: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.INTEGER,
+        defaultValue:1
+      },
+      
+     
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Categories');
+  }
+};
